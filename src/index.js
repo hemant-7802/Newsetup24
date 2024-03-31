2515ee30ae5ab5db00b88d163be3b47d56ed449d
Hemant Bhargav <hsharma9667@gmail.com>
2024-04-01 00:02:18 +0530

    second commit ignore files added

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import { Express } from "express";
+import { db_name } from "./constant.js"
+import dotenv from "dotenv";
+
+const app = Express()
+
+;(async () => {
+
+} )()
